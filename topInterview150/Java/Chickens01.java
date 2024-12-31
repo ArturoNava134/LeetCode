796b324f7989dd1fcd16cd44cfcdb4276707d4dd
ArturoNava134 <arturonava940@gmail.com>
2024-12-31 17:14:38 -0600

    chickens01Documented

--- topInterview150/Java/Chickens01.java
+++ topInterview150/Java/Chickens01.java
@@ -7,7 +7,7 @@ public class Chickens01 {
         int eggsPerChicken = 4;
         int chickenCount = 8;
         int totalEggs = 0;
-        
+        // you can uptdate the count of the days by increasing the number into the i<= of the loop
         for (int i = 0; i<=2; i++){
             int day = 1+i;
             // System.out.println("day of the week: " + day);
