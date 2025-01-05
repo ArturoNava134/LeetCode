b1d02239b9f0d5d6ecd82f031f9fa55e54c73c68
Art134 <aser_sd@gmail.com>
2025-01-04 23:36:01 -0600

    56mergeIntervals2

--- topInterview150/JS/56mergeIntervals.js
+++ topInterview150/JS/56mergeIntervals.js
@@ -1,6 +1,6 @@
 // Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
 
- 
+//huh
 
 // Example 1:
 
