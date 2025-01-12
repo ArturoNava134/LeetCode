a0d67df6dd8d6fcd1bd43fe868fdcaf9d7833004
ArturoNava134 <arturonava940@gmail.com>
2025-01-11 22:11:57 -0600

    70climbingStairsJS

--- topInterview150/JS/70climbingStairs.js
+++ topInterview150/JS/70climbingStairs.js
@@ -1,12 +1,14 @@
 var climbStairs = function(n, memo = {1:1, 2:2}) {
     if (memo[n] !== undefined) return memo[n];   
     memo[n] = climbStairs(n - 1, memo) + climbStairs(n - 2, memo);
-    
+
     console.log(memo[n]);
     return memo[n];
  };
 
  n = 2
 
+
+ 
 
 climbStairs(n)
