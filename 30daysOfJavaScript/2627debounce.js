98e36cc89260b8af850d4af140a985dc603019d6
ArturoNava134 <arturonava940@gmail.com>
2025-04-18 23:12:59 -0600

    2627debounce

---
+++ 30daysOfJavaScript/2627debounce.js
@@ -0,0 +1,19 @@
+var debounce = function(fn, t) {
+    let id;
+    return function(...args) {
+        clearTimeout(id);
+        id = setTimeout(() => fn(...args), t);
+    }
+};
+
+
+let start = Date.now();
+function log(...inputs) { 
+  console.log([Date.now() - start, inputs]);
+}
+
+const dlog = debounce(log, 50);
+
+
+setTimeout(() => dlog(1), 50); 
+setTimeout(() => dlog(2), 75); 
