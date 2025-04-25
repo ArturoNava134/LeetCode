fa49283a5ecfcefb552b88924a10cdbde481ef79
ArturoNava134 <arturonava940@gmail.com>
2025-04-24 23:12:55 -0600

    2632groupBy

---
+++ 30daysOfJavaScript/2632groupBy.js
@@ -0,0 +1,13 @@
+Array.prototype.groupBy = function(fn) {
+    const res = {};
+    
+    for(const obj of this){
+        const key = fn(obj);
+        if(!res.hasOwnProperty(key)){
+            res[key] = [];
+        }
+        res[key].push(obj);
+    }
+
+    return res;
+};
