930a7f3a6596cbf81b73c4fd843cc7376e4cee36
ArturoNava134 <arturonava940@gmail.com>
2025-07-07 19:55:58 -0600

    110balancedBinaryTree

---
+++ randomProblems/python/110balancedBinaryTree.py
@@ -0,0 +1,19 @@
+class Solution:
+    def __init__(self):
+        self.flag = True
+    def check(self, root):
+        if not root:
+            return 0
+        left = self.check(root.left)
+        right = self.check(root.right)
+        if abs(left - right) > 1:
+            self.flag = False
+        return 1 + max(left, right)
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        self.check(root)
+        if self.flag:
+            return True
+        return False
+        
