0ac5c5b41a61e29a502deb1be341e048ee29c091
ArturoNava134 <arturonava940@gmail.com>
2025-08-20 22:34:47 -0600

    twoalgorithms

--- randomProblems/js/1twosum.js
+++ randomProblems/js/1twosum.js
@@ -4,11 +4,30 @@ function twoSum(nums, target) {
 
   for (let i = 0; i < nums.length; i++) {
     const complementIndex = complements.get(nums[i]);
+    console.log(complementIndex);
     if (complementIndex !== undefined) {
-     
+      console.log(i, complementIndex);
       return [i, complementIndex];
     }
     complements.set(target - nums[i], i);
   }
   return []; 
 }
+
+// function twoSum(nums, target){
+//   for (let i = 0; i < nums.length; i++){
+//     for(let j = i + 1; j < nums.length; j++){
+//       console.log("i: ", i);
+//       console.log("j: ", j);
+//       if(nums[i] + nums[j] === target){
+//         return [i, j];
+//       }
+//     }
+//   }
+// }
+
+let n1=[2,7,11,15], n2=[3,2,4];
+let target1 = 9, target2=6;
+
+twoSum(n2,target2);
+
